a3566f7fa01f839ddc73c996e653b855f2db5778
shaikh22-2024 <shaikh22@mcmaster.ca>
2024-12-02 15:59:32 -0500

    second found changed to bool

--- Food.cpp
+++ Food.cpp
@@ -20,28 +20,31 @@ void Food::generateFood(objPosArrayList& blockOff)
 {
     srand(time(NULL));
 
+    //Reomve all food objects from the gameboard everytime we want to generate new food positions after the snake collides with a food object. . 
     for (int i = 0; i < numFood; i++) {
         foodBucket->removeTail();
     }
 
+    //intialize every element of the string with the possible food objects. 
     for (int i = 0; i < numFood; i++) {
         char symbol = 'o';
         if (i == numFood - 1) {
             symbol = SPECIAL_FOOD_CHARACTER;
         }
 
+        //Create an object for new position of the food at a random x & y coord and with the symbol,
         objPos newPos(1 + rand() % (xMax - 1), 1 + rand() % (yMax - 1), symbol);
 
         while (true) {
             newPos = objPos(1 + rand() % (xMax - 1), 1 + rand() % (yMax - 1), symbol);
 
             // Check that we're not creating a food item on the snake
-            int found = 0;
+            bool found = false;
 
             for (int i = 0; i < blockOff.getSize(); i++) {
                 objPos currentPos = blockOff.getElement(i);
                 if (newPos.isPosEqual(&currentPos)) {
-                    found = 1;
+                    found = true;
                     break;
                 }
             }
@@ -54,7 +57,7 @@ void Food::generateFood(objPosArrayList& blockOff)
             for (int i = 0; i < foodBucket->getSize(); i++) {
                 objPos currentPos = foodBucket->getElement(i);
                 if (newPos.isPosEqual(&currentPos)) {
-                    found = 1;
+                    found = true;
                     break;
                 }
             }
